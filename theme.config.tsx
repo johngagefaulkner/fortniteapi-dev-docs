fbe999cfed3b143c372e55269f21c06778a0ff6d
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2023-01-28 01:52:52 -0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>My Project</span>,
+	project: {
+		link: "https://github.com/shuding/nextra-docs-template",
+	},
+	chat: {
+		link: "https://discord.com",
+	},
+	docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+	footer: {
+		text: "2023 ©️ FortniteAPI.dev",
+	},
+};
 
-export default config
+export default config;
